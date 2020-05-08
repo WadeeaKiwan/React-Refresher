f88ed8ea574ee83bdd0c7fbb9ad6c29633610e80
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-08 04:16:13 +0200

    Split code to components & use props to pass data between components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import GoalList from "./components/GoalList";
+import "./App.css";
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the course Q&A" }
+  ];
+
+  return (
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
+
+// const App = () => {
+//   // return <h1 title="This works!">A React App!</h1>;
+//   // return React.createElement("h1", { title: "This works" }, "Hi from react");
+//   return React.createElement(
+//     "div",
+//     { title: "This works" },
+//     React.createElement("h1", { title: "This works" }, "Hi from react")
+//   );
+// };
+
+// class App extends React.Component {
+//   render() {
+//     return <h1 title='This works!'>A React App!</h1>;
+//   }
+// }
 
 export default App;
